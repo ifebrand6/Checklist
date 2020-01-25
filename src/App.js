aa3fcf0a8cecbd2a9dc7a22b6f48d62fd1626f78
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-25 23:32:43 +0100

    added app features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import Todo from "./Todo";
+import Task from "./Task";
+import Addtask from './addTask';
 
 class App extends Component {
   state = {
-    todos: [
+    tasks: [
       {id: 1, title: 'finish react course'},
       {id:2, title: 'check for upwork.'}
     ]
   }
+  addTask = (task) => {
+    task.id = Math.floor(Math.random() * 100);
+    let tasks = [...this.state.tasks, task]
+    this.setState({
+      tasks
+    })
+   console.log(task)
+   
+  }
+  deleteTask = (id) =>{
+    console.log(id)
+    let tasks = this.state.tasks.filter(task => {
+       return task.id !== id;
+    })
+    this.setState({
+      // tasks: tasks
+      tasks
+    }
+    )
+  }
  render(){
    return(
-      <div className="todo">
-        <h1> Check List</h1>
-        <Todo className="container" todo={this.state.todos} />
+      <div className="task-app container">
+        <h1 className="center blue-text"> Check List</h1>
+        <Task className="container" tasks={this.state.tasks} deleteTask={this.deleteTask} />
+        <Addtask addTask={this.addTask}/>
       </div>
    );
  }
